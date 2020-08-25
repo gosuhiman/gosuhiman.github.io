d78bc0057eec8ce4655f329ba345fc32b61a065e
gosuhiman <gosuhiman@gmail.com>
2020-08-25 13:45:48 +0200

    Code reformatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {MainSkillsComponent} from "./main-skills/main-skills.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainSkillsComponent } from './main-skills/main-skills.component';
 
 const routes: Routes = [
-  {path: '', component: MainSkillsComponent},
+  { path: '', component: MainSkillsComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {TopBarComponent} from './top-bar/top-bar.component';
-import {SocialLinksComponent} from './social-links/social-links.component';
-import {MainSkillsComponent} from './main-skills/main-skills.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { MainSkillsComponent } from './main-skills/main-skills.component';
 
 @NgModule({
   declarations: [
